d997aecd10ad38584922a7454d1bf0a28152edb6
Domingo <dmm.martin@gmail.com>
2017-04-13 20:07:10 +0200

    clean up tape class

--- Tape.js
+++ Tape.js
@@ -19,36 +19,12 @@ class Tape {
     }
     
     record(delay) {
-        
-            var pin   = 36;
-            var count = 0;
-            var max   = 1;
-
-            //gpio.setup(pin, gpio.DIR_OUT, on);
-
-        on();
-        
-        function on() {
-            if (count >= max) {
-                gpio.destroy(function() {
-                    console.log('Closed pins, now exit');
-                });
-                return;
-            }
-
-            setTimeout(function() {
-                //gpio.write(pin, true, on);
-                //on();
-                //count += 1;
-                gpio.destroy(function() {
-                    console.log('Closed pins, now exit');
-                });
-            }, delay);
-        }
-       
-
+        setTimeout(function() {
+            gpio.destroy(function() {
+                console.log('Closed pins, now exit');
+            });
+        }, delay);
     }
-    
 }
 
 module.exports = new Tape();
