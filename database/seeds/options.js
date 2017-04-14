089a7def0a6ab85bc4b69e819e95248430c1612e
Domingo <dmm.martin@gmail.com>
2017-04-15 01:31:19 +0200

    add options seed

---
+++ database/seeds/options.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Options = {
+            env: 'production',
+            productionUrl: 'https://watape.com/',
+            devUrl: 'http://localhost/laravel/taptapo/public',
+            tapeOn: true,
+            tapeFinished: false,
+            timeTapeRecorded: 0
+            };
+
+module.exports = Options;
